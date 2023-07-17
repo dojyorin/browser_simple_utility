b64ee0011b1b827738f64dea71672506eba5cdc8
dojyorin <ota_droid@live.jp>
2023-07-18 04:38:39 +0900

    WIP.

--- src/notify.ts
+++ src/notify.ts
@@ -3,11 +3,15 @@
 /// <reference lib="dom"/>
 /// <reference lib="dom.iterable"/>
 
-export function dialogNotify(message:string, color:number):void{
-    if(0x00FFFFFF < color){
-        throw new RangeError();
-    }
-
+/**
+* Display fixed toast at top of screen.
+* Display for `5000` ms by default.
+* @example
+* ```ts
+* dialogNotify("Successfully!", "#13AD35");
+* ```
+*/
+export function dialogNotify(message:string, color:string, time?:number):void{
     const dialog = document.createElement("dialog");
     dialog.style.position = "fixed";
     dialog.style.zIndex = "255";
@@ -17,12 +21,12 @@ export function dialogNotify(message:string, color:number):void{
     dialog.style.border = "0";
     dialog.style.borderRadius = "8px";
     dialog.style.fontSize = "16px";
-    dialog.style.backgroundColor = `#${color.toString(16).padStart(6, "0")}`;
+    dialog.style.backgroundColor = color;
     dialog.style.boxShadow = "0 2px 4px 0 #000000";
     dialog.style.overflowWrap = "break-word";
     dialog.textContent = message;
     dialog.show();
 
-    setTimeout(() => dialog.remove(), 5000);
+    setTimeout(() => dialog.remove(), time ?? 5000);
     document.body.appendChild(dialog);
 }
