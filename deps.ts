e4bf1be0620eab6d11da79e866aac22a90823615
dojyorin <ota_droid@live.jp>
2023-07-25 22:36:30 +0900

    update deps.

--- deps.ts
+++ deps.ts
@@ -1,2 +1,2 @@
-// @deno-types="https://deno.land/x/simple_utility@v1.3.0/mod.ts"
-export {type FileInit, base64Encode} from "https://esm.sh/gh/dojyorin/deno_simple_utility@v1.3.0/mod.universal.ts?bundle&target=esnext";
+// @deno-types="https://deno.land/x/simple_utility@v1.3.1/mod.ts"
+export {type FileInit, base64Encode} from "https://esm.sh/gh/dojyorin/deno_simple_utility@v1.3.1/mod.universal.ts?bundle&target=esnext";
