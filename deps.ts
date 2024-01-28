acb310e0a8a5ff60e392459db4f55bbe70d76052
dojyorin <ota_droid@live.jp>
2024-01-29 02:35:25 +0900

    npm protocol.

--- deps.ts
+++ deps.ts
@@ -6,4 +6,4 @@
 // @deno-types="https://deno.land/x/simple_utility@v2.0.0/mod.ts"
 export {type DataMap, blobConvert} from "https://esm.sh/gh/dojyorin/deno_simple_utility@v2.0.0/mod.pure.ts?bundle&target=esnext";
 
-export type * from "https://esm.sh/@types/wicg-file-system-access@2023.10.4/index.d.ts";
+export type * from "npm:@types/wicg-file-system-access@2023.10.4/index.d.ts";
