99603f4e06cde8c45c8174fb968285c328bd2716
dojyorin <ota_droid@live.jp>
2024-10-29 20:42:14 +0900

    add loading.

--- src/dialog.ts
+++ src/dialog.ts
@@ -3,10 +3,10 @@
 * Default display time is 5000ms.
 * @example
 * ```ts
-* dialogNotify("Complete!", "green");
+* showNotify("Complete!", "green");
 * ```
 */
-export function dialogNotify(message: string, color: string, time?: number): void {
+export function showNotify(message: string, color: string, time?: number): void {
     const dialog = document.createElement("dialog");
     dialog.textContent = message;
 
@@ -36,4 +36,49 @@ export function dialogNotify(message: string, color: string, time?: number): voi
     setTimeout(() => dialog.remove(), time ?? 5000);
 
     dialog.show();
+}
+
+/**
+* Show loading circle modally.
+* Insert into DOM with factory function and control on/off with returned function.
+* @example
+* ```ts
+* const showLoading = factoryLoading("blue");
+* showLoading(true);
+* ```
+*/
+export function factoryLoading(color: string): (enable: boolean) => void {
+    const dialog = document.createElement("dialog");
+    const style = document.createElement("style");
+    style.textContent = /*css*/`
+        @scope {
+            @keyframes loading {
+                to {
+                    transform: rotate(1turn);
+                }
+            }
+
+            & {
+                padding: 8px;
+                height: 80px;
+                aspect-ratio: 1;
+                outline: none;
+                border: none;
+                border-radius: 50%;
+                background-color: ${color};
+                mask: conic-gradient(transparent 30%, #000000), linear-gradient(#000000 0 0) content-box;
+                mask-composite: subtract;
+                animation: loading 1s infinite linear;
+
+                &::backdrop {
+                    background-color: #000000A0;
+                }
+            }
+        }
+    `;
+
+    dialog.appendChild(style);
+    document.body.appendChild(dialog);
+
+    return enable => enable ? dialog.showModal() : dialog.close();
 }
