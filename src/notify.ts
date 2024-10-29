b6d3648cbfa030e35be37b9a87788987db9d9473
dojyorin <ota_droid@live.jp>
2024-10-29 20:25:07 +0900

    wip

--- src/notify.ts
+++ src/notify.ts
@@ -8,20 +8,32 @@
 */
 export function dialogNotify(message: string, color: string, time?: number): void {
     const dialog = document.createElement("dialog");
-    dialog.style.position = "fixed";
-    dialog.style.zIndex = "255";
-    dialog.style.top = "16px";
-    dialog.style.padding = "8px 16px";
-    dialog.style.maxWidth = "calc(100dvw - 20px)";
-    dialog.style.border = "none";
-    dialog.style.borderRadius = "8px";
-    dialog.style.fontSize = "16px";
-    dialog.style.backgroundColor = color;
-    dialog.style.boxShadow = "0 2px 4px 0 #000000";
-    dialog.style.overflowWrap = "break-word";
     dialog.textContent = message;
-    dialog.show();
+
+    const style = document.createElement("style");
+    style.textContent = /*css*/`
+        @scope {
+            & {
+                position: fixed;
+                z-index: 255;
+                top: 20px;
+                padding: 8px 16px;
+                max-width: calc(100dvw - 20px);
+                outline: none;
+                border: none;
+                border-radius: 8px;
+                font-size: 16px;
+                background-color: ${color};
+                box-shadow: 0 2px 4px 0 #000000;
+                overflow-wrap: break-word;
+            }
+        }
+    `;
+
+    dialog.appendChild(style);
+    document.body.appendChild(dialog);
 
     setTimeout(() => dialog.remove(), time ?? 5000);
-    document.body.appendChild(dialog);
+
+    dialog.show();
 }
